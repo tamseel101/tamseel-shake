1e47a7e1b2010008efb2e5e5beb4a893a39cf430
tamseel101 <78289413+tamseel101@users.noreply.github.com>
2024-10-04 11:17:40 -0400

    added more changes

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Harris johnsen. <br />
-        All rights reserved.
+        &copy; 2024 Tamseel Sheikh. <br />
+        {/* All rights reserved. */}
       </p>
     </section>
   );
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Tamseel Ahmed"
         />
         <img
           className={styles.colorMode}
@@ -36,19 +36,19 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Tamseel
           <br />
-          Johnsen
+          Sheikh
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
+          {/* <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
-          </a>
-          <a href="https://github.com/" target="_blank">
+          </a> */}
+          <a href="https://github.com/tamseel101" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/tamseelsheikh/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
